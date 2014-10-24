4b23a2bd18bb8e6ea18d262a1db53ac626ece75f
Eric K Idema <eki@vying.org>
2014-10-24 15:39:00 -0400

    Attempt to determine whether a response is cacheable.

--- lib/agentx/response.rb
+++ lib/agentx/response.rb
@@ -16,6 +16,27 @@ module AgentX
       Array(headers.set_cookie || [])
     end
 
+    def ok?
+      code == 200
+    end
+
+    def not_modified?
+      code == 304
+    end
+
+    def fresh?
+      headers.ttl && headers.ttl > 0
+    end
+
+    CACHEABLE_CODES = [200, 203, 300, 301, 302, 404, 410]
+
+    def cacheable?
+      return false if headers.cache_control && headers.cache_control.no_store?
+      return false unless CACHEABLE_CODES.include?(code)
+
+      !! (headers.etag || headers.last_modified || fresh?)
+    end
+
     def inspect
       "(Response #{code})"
     end
@@ -40,8 +61,11 @@ module AgentX
     end
 
     class Headers
+      include Enumerable
+
       def initialize(hash={})
         @hash = hash
+        @hash['Date'] ||= Time.now.httpdate
         @normalized = {}
         @hash.each do |k,v|
           @normalized[k.to_s.downcase] = v
@@ -56,15 +80,26 @@ module AgentX
         str.lines.each do |line|
           next if line =~ /^HTTP\/\d/
           k, v = line.split(':', 2).map { |s| s.strip }
-          if hash[k]
-            hash[k] = Array(hash[k])
-            hash[k] << v
-          else
-            hash[k] = v
+
+          if k && v
+            if hash[k]
+              hash[k] = Array(hash[k])
+              hash[k] << v
+            else
+              hash[k] = v
+            end
           end
         end
 
         new(hash)
+      end
+
+      def inspect
+        "(Headers #{@normalized})"
+      end
+
+      def each(&block)
+        @normalized.each(&block)
       end
 
       def [](k)
@@ -76,9 +111,35 @@ module AgentX
       end
 
       def date
-        if d = @normalized['date']
+        Time.parse(@normalized['date'])
+      end
+
+      def age
+        (@normalized['age'] || (Time.now - date)).to_i
+      end
+
+      def expires
+        if d = @normalized['expires']
           Time.parse(d)
         end
+      end
+
+      def max_age
+        (cache_control && 
+         cache_control.shared_max_age || cache_control.max_age) ||
+        (expires && (expires - Time.now))
+      end
+
+      def ttl
+        max_age && (max_age - age)
+      end
+
+      def last_modified
+        @normalized['last-modified']
+      end
+
+      def etag
+        @normalized['etag']
       end
 
       def content_type
@@ -103,6 +164,12 @@ module AgentX
         end
       end
 
+      def cache_control
+        if @normalized['cache-control']
+          @cache_control ||= CacheControl.parse(@normalized['cache-control'])
+        end
+      end
+
       def set_cookie
         @normalized['set-cookie']
       end
@@ -113,6 +180,62 @@ module AgentX
 
       def to_hash
         @hash
+      end
+
+      class CacheControl
+        attr_reader :directives
+
+        def initialize(directives)
+          @directives = directives
+        end
+
+        def public?
+          @directives['public']
+        end
+
+        def private?
+          @directives['private']
+        end
+
+        def no_store?
+          @directives['no-store']
+        end
+
+        def no_cache?
+          @directives['no-cache']
+        end
+
+        def must_revalidate?
+          @directives['must-revalidate']
+        end
+
+        def max_age
+          @directives['max-age'] && @directives['max-age'].to_i
+        end
+
+        def shared_max_age
+          @directives['s-max-age'] && @directives['s-max-age'].to_i
+        end
+
+        def to_s
+          directives.map { |k,v| v == true ? k : "#{k}=#{v}" }.join(', ')
+        end
+
+        def inspect
+          "(CacheControl #{directives})"
+        end
+
+        def self.parse(s)
+          h = {}
+
+          s.gsub(' ', '').split(',').each do |p|
+            k, v = p.split('=')
+
+            h[k.downcase] = (v || true)
+          end
+
+          new(h)
+        end
       end
     end
   end
